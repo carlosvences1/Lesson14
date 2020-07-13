07ff7eced64b17a3381263b093c8e7f84f04abdf
carlosvences1 <54856863+carlosvences1@users.noreply.github.com>
2020-07-12 22:49:23 -0400

    upload

---
+++ precache-manifest.7e9c90e5fd14bc2176a2b4c63b4a512d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223b1397bb70707eb2fe91c5f09977bd",
+    "url": "/jcvences/web215/lesson14/index.html"
+  },
+  {
+    "revision": "169a0d666dc5d2ac49b9",
+    "url": "/jcvences/web215/lesson14/static/css/main.9e5f9fa2.chunk.css"
+  },
+  {
+    "revision": "59379e738cb412c3b538",
+    "url": "/jcvences/web215/lesson14/static/js/2.02450a35.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/jcvences/web215/lesson14/static/js/2.02450a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "169a0d666dc5d2ac49b9",
+    "url": "/jcvences/web215/lesson14/static/js/main.6ea70a24.chunk.js"
+  },
+  {
+    "revision": "2830975732994f877088",
+    "url": "/jcvences/web215/lesson14/static/js/runtime-main.0bfc0b8d.js"
+  },
+  {
+    "revision": "c791ae84852b6746ce62a240cad8c06c",
+    "url": "/jcvences/web215/lesson14/static/media/headshot.c791ae84.jpg"
+  }
+]);
